b198c9fea3bc85cd3df891c90ac628120c9c4835
CorneliusCornbread <jrosenbeckerdev@gmail.com>
2024-11-18 13:50:59 -0600

    WIP

--- Code/StateSpace.py
+++ Code/StateSpace.py
@@ -3,11 +3,24 @@ State Space Model
 TODO: add info
 """
 
+from typing import List
 from Model import Model
+import torch
+import torch.nn as nn
+from torch.nn import functional
+
+MATRIX_SIZE = 10
 
 class StateSpace(Model):
-    def fit(self):
+    def fit(self, data: List[str]):
+        self.weight_dimensions = MATRIX_SIZE
+        self.weight_a = self.create_default_matrix()
+        self.weight_b = self.create_default_matrix()
+        self.state = self.create_default_matrix()
         pass
 
     def generate(self, phrase):
         pass
+
+    def create_default_matrix(self):
+        return nn.Parameter(torch.randn(self.weight_dimensions, self.weight_dimensions));
